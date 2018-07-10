22e81be97748f42ce4f43a2d72cc901e74cea954
Xinyang Zhang <xinyang.zhang@hpe.com>
2018-07-10 15:46:05 +0800

    cors

--- index.js
+++ index.js
@@ -1,44 +1,30 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const jwt = require('jsonwebtoken');
+const expressjwt = require('express-jwt');
+const cors = require('cors');
 
 const app = express();
 const PORT = process.env.PORT || 8888;
-const users = [
-  {id: 1, username: 'admin', password: 'admin'},
-  {id: 2, username: 'guest', password: 'guest'}
-]
 
 app.use(bodyParser.json());
+app.use(cors());
 
-app.post('/login', (req, res) => {
-  if(!req.body.username || !req.body.password) {
-    res
-      .status(400)
-      .send(`You need a username and password`);
-      return;
-  }
+const jwtCheck = expressjwt({
+  secret: "mysupersecretkey"
+});
 
-  const user = users.find(u => {
-    return u.username === req.body.username && u.password === req.body.password;
-  });
-
-  if(!user) {
-    res
-      .status(401)
-      .send(`User not found`);
-      return;
-  }
-
-  const token = jwt.sign({
-    sub: user.id,
-    username: user.username
-  }, 'mysupersecretkey', { expiresIn: '3 hours'});
-
+app.get('/resource', (req, res) => {
   res
     .status(200)
-    .send({access_token: token});
+    .send('Public resource, you can see this');
+});
+
+app.get('/resource/secret', jwtCheck, (req, res) => {
+  res
+    .status(200)
+    .send('Secret resource, you should be logged in to see this');
 })
+
 
 app.get('/status', (req, res) => {
   const localTime = (new Date()).toLocaleTimeString();
